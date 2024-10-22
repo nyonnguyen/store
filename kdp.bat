3d65eb53d2a5eb6e89a54d4753dd26eb4e0dd298
nhnguyen <nhnguyen@axon.com>
2024-10-22 18:26:29 +0700

    add bat file supports run on windows

--- kdp.bat
+++ kdp.bat
@@ -4,26 +4,25 @@ cd /d C:\Users\Nyon\code\kdp
 
 REM Check if venv folder exists, if not, install requirements and npm dependencies
 if not exist "venv\" (
-    echo Virtual environment not found, installing dependencies...
+    echo Virtual environment not found, Create and installing dependencies...
     
     REM Create a virtual environment if needed (ensure Python and virtualenv are installed)
     python -m venv venv
 
-    REM Activate the virtual environment
-    call venv\Scripts\activate
-
-    REM Install Python dependencies
-    pip install -r requirements.txt
-) else (
-    REM Activate the virtual environment
-    call venv\Scripts\activate
 )
 
+REM Activate the virtual environment
+call venv\Scripts\activate
+
+REM Install Python dependencies
+pip install -r requirements.txt
+	
+echo
 REM Navigate to the frontend folder and run npm install
 cd web_app\frontend
 if not exist "node_modules\" (
     echo Installing npm dependencies...
-    npm install
+    call npm install
 ) else (
 	Echo found node_modules
 )
